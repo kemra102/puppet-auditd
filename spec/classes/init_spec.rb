64e51cf335095d22445d7a296b088d406712d2eb
Danny Roberts <danny@thefallenphoenix.net>
2015-05-20 23:31:57 +0100

    Full test added back for RHEL 7 system with defaults

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,34 @@
 require 'spec_helper'
   describe 'auditd', :type => :class do
-    context 'with defaults for all parameters' do
+    context 'when called with no parameters on RedHat 7' do
       let (:facts) {{
         :osfamily               => 'RedHat',
         :operatingsystemrelease => '7',
       }}
-      it { should contain_class('auditd') }
+      it { 
+        should contain_class('auditd')
+        should contain_package('audit').with({
+          'ensure' => 'present',
+        })
+        should contain_file('/etc/audit/auditd.conf').with({
+          'ensure' => 'file',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0640',
+        })
+        should contain_file('/etc/audit/audit.rules').with({
+          'ensure' => 'file',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0640',
+        })
+        should contain_service('auditd').with({
+          'ensure'    => 'running',
+          'enable'    => 'true',
+          'hasstatus' => 'true',
+          'restart'   => '/usr/libexec/initscripts/legacy-actions/auditd/restart',
+          'stop'      => '/usr/libexec/initscripts/legacy-actions/auditd/stop',
+        })
+      }
     end
 end
