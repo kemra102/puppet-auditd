2e2108aca00cb2d035a25869ab456c0e404d0326
Danny Roberts <danny@thefallenphoenix.net>
2015-05-20 23:19:30 +0100

    Basic test working

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
-describe 'auditd' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('auditd') }
-  end
+  describe 'auditd', :type => :class do
+    context 'with defaults for all parameters' do
+      let (:facts) {{
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '7',
+      }}
+      it { should contain_class('auditd') }
+    end
 end
