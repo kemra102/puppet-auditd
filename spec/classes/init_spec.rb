9958c76401085e96bf1b70bc059770b93c2585a0
Danny Roberts <danny@thefallenphoenix.net>
2015-10-11 13:49:44 +0100

    Fix tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,7 +9,7 @@ describe 'auditd', :type => :class do
     it { 
       should contain_class('auditd')
       should contain_package('auditd').with({
-        'ensure' => 'absent',
+        'ensure' => 'present',
         'name'   => 'audit',
       })
       should contain_file('/etc/audit/auditd.conf').with({
